eb1575fd8ec2328afea210b64515203f29165915
Armel <armelknyobe@gmail.com>
2023-12-29 16:11:14 +0100

    separate test and package steps

---
+++ src/main/java/com/binark/querypredicate/App.java
@@ -0,0 +1,9 @@
+package com.binark.querypredicate;
+
+public class App {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/test/java/com/binark/querypredicate/AppTest.java
@@ -0,0 +1,9 @@
+package com.binark.querypredicate;
+
+public class AppTest {
+
+  public static void main(String[] args) {
+
+  }
+
+}
